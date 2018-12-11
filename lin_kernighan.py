4ac639783674f6ecfafca8bf6f73ffdf0c6cbdb8
tylerforrester <forrestt@oregonstate.edu>
2018-12-10 21:28:13 -0700

    Added comments to lin_kernighan.py

--- lin_kernighan.py
+++ lin_kernighan.py
@@ -1,3 +1,7 @@
+
+"""CSCI 538 Project Traveling Salesman
+   Tyler Forrester  December 10th 2018"""
+
 import numpy as np
 import logging
 
@@ -6,6 +10,11 @@ np.random.seed(1)
 
 
 def get_first_tour(num_cities):
+    """
+
+    :param num_cities: Number of vertexs in graph
+    :return: Random Tour of Graph
+    """
     return (np.random.choice(num_cities, num_cities, replace=False))
 
 
@@ -13,7 +22,7 @@ def get_tour_distance(tour, points):
     """
 
     :param tour:[4,2,3,1]
-    :return:
+    :return: Measures the Euclidean distance of the tour
     """
     distance = 0
     for i in range(len(tour) - 1):
@@ -24,6 +33,16 @@ def get_tour_distance(tour, points):
 
 
 def test_swap(i, j, old_tour, best_distance, points):
+    """
+    Function which swaps points and tests if the new tour is better than that old tour.
+    In this variation the tour is
+    :param i: Start of swap index
+    :param j: End of swap index
+    :param old_tour: The current tour
+    :param best_distance: The current lowest distance founds
+    :param points: 2-D points to measure euclidean distance
+    :return: The current shortest tour and it's distance.
+    """
     new_tour = np.copy(old_tour)
 
     rev_end = i - 1
@@ -46,26 +65,36 @@ def test_swap(i, j, old_tour, best_distance, points):
 
 
 def do2opt(tour, points):
-    num_cities = len(tour)
-    old_distance = get_tour_distance(tour, points)
+    """
 
-    for i in range(num_cities - 1):
-        for j in range(i + 1, num_cities):
-            tour, distance = test_swap(i, j, tour, old_distance, points)
-            # if distance == old_distance:
-            #     return tour, distance
+    :param tour: The random tour
+    :param points: Location in Euclidean space
+    :return: Best tour and distance
+    """
+    num_vertices = len(tour)
+    max_distance = get_tour_distance(tour, points)
 
-            old_distance = distance
+    for i in range(num_vertices - 1):
+        for j in range(i + 1, num_vertices):
+            tour, distance = test_swap(i, j, tour, max_distance, points)
+
+            max_distance = distance
 
             logging.debug("Tour {} Distance {}".format(tour, distance))
 
-    return tour, old_distance
+    return tour, max_distance
+
+
+
 
 
 if __name__ == "__main__":
+
+    # generate random ten city graph.
     num_cities = 10
     points = np.random.randint(0, 1000, size=(num_cities, 2))
     logging.debug(points)
+
     starting_tour = get_first_tour(num_cities)
     print(do2opt(starting_tour, points))
 
